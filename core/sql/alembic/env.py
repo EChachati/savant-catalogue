51976da0059635b8ace68e83bd2aa3a64acb5143
EChachati <EChachati@users.noreply.github.com>
2023-12-22 13:55:49 +0000

    Ruff Action -> Fixing linting errors

--- core/sql/alembic/env.py
+++ core/sql/alembic/env.py
@@ -6,7 +6,13 @@ from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
-from core.sql.models import Category, Company, Product, Purchase, PurchaseProductLink  # noqa: F401
+from core.sql.models import (  # noqa: F401
+    Category,
+    Company,
+    Product,
+    Purchase,
+    PurchaseProductLink,
+)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
