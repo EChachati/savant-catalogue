37fb0c4451906e9ac31f27f3e0eb6fd52c14fae6
Edkar Chachati <chachati28@gmail.com>
2023-12-21 23:50:58 -0400

    rename function

--- core/sql/database.py
+++ core/sql/database.py
@@ -1,13 +1,15 @@
 import os
 
+from dotenv import load_dotenv
 from sqlmodel import Session, create_engine
 
+load_dotenv()
 DATABASE_URL = os.environ["DB_URI"]
 
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 
-def get_session():
+def get_db():
     with Session(engine) as session:
         yield session
