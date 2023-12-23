d2b18dee0b1a28e93ea557e6fc0349a00bdd4f54
Edkar Chachati <chachati28@gmail.com>
2023-12-23 17:13:18 -0400

    formatting

--- core/sql/database.py
+++ core/sql/database.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from sqlmodel import Session, create_engine
 
 load_dotenv()
-DATABASE_URL = os.environ["DB_URI"]
 
+DATABASE_URL = os.environ["DB_URI"]
 
 engine = create_engine(DATABASE_URL, echo=True)
 
