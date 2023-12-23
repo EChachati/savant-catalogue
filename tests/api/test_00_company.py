da40239f60c29db32ba7d1f03b37609fd518d515
Edkar Chachati <chachati28@gmail.com>
2023-12-23 17:15:42 -0400

    Update Tests

--- tests/api/test_00_company.py
+++ tests/api/test_00_company.py
@@ -1,6 +1,10 @@
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+from sqlmodel import Session, select
 
+from core.sql.database import engine
+from core.sql.models import Company
 from main import app
 
 client = TestClient(app)
@@ -20,14 +24,24 @@ data = {
 }
 
 
+@pytest.fixture(name="get_last_id")
+def get_last_id_fixture():
+    return (
+        Session(engine)
+        .exec(select(Company).order_by(Company.id.desc()))
+        .first()
+        .id
+    )
+
+
 def test_create_company():
     response = client.post(path["create"], json=data)
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["name"] == data["name"]
 
 
-def test_get_company():
-    response = client.get(path["get"])
+def test_get_company(get_last_id):
+    response = client.get(BASE_PATH + str(get_last_id))
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["id"] == 1
 
@@ -41,19 +55,19 @@ def test_list_company():
     assert "name" in response.json()[0]
 
 
-def test_update_company():
-    data.update({"name": "TotallyNotSavant"})
+def test_update_company(get_last_id):
+    data.update({"name": "TotallyNotSavant", "id": get_last_id})
     response = client.put(path["update"], json=data)
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["name"] == data["name"]
 
 
-def test_delete_company():
-    response = client.delete(path["delete"])
+def test_delete_company(get_last_id):
+    response = client.delete(BASE_PATH + str(get_last_id))
 
     assert response.status_code == status.HTTP_200_OK
     assert isinstance(response.json(), dict)
 
-    path.update({"delete": BASE_PATH + "99999"})
+    response = client.delete(BASE_PATH + str(999999))
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert isinstance(response.json(), dict)
---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import os
+
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def make_migration():
+    os.system("alembic upgrade head")
+    return
